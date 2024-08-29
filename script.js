a362ab09b712363f38002df188045c07240fd199
sujal2005code <143809728+sujal2005code@users.noreply.github.com>
2024-08-29 21:24:03 +0530

    Add files via upload
    codes

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+function addToCart(item) {
+    alert(item + " has been added to your cart!");
+    // Add further logic here to handle cart functionality
+}
+
+function buyNow(item) {
+    alert("Proceeding to buy " + item + "!");
+    // Add further logic here to handle the purchase
+}
