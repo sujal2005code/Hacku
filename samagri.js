ea540123d34215071f6a93e49c837f64a0f6a6ca
sujal2005code <sujalmaity1800@gmail.com>
2024-08-30 14:51:45 +0530

    bahut changes kare hai

---
+++ samagri.js
@@ -0,0 +1,10 @@
+
+function addToCart(item) {
+    alert(item + " has been added to your cart!");
+    // Add further logic here to handle cart functionality
+}
+
+function buyNow(item) {
+    alert("Proceeding to buy " + item + "!");
+    // Add further logic here to handle the purchase
+}
